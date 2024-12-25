584e7218bd0baaaa7241f0c78d613c9b1478c4ff
VLTHellolin <i@hellolin.top>
2024-12-25 19:24:29 +0800

    chore: add polyfill lib

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '@/styles/globals.css';
+import 'core-js/actual';
+
 import { NextUIProvider } from '@nextui-org/react';
 import App from './App.tsx';
 
