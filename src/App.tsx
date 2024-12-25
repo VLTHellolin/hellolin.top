32449fe57ca240d6704146bba1712423ccdf42c9
VLTHellolin <i@hellolin.top>
2024-12-25 20:42:57 +0800

    feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Background } from './components/Background';
 import { Container } from './components/Container';
+import { Footer } from './components/Footer';
 import { IntroCard } from './components/IntroCard';
 import { ProjectCard } from './components/ProjectCard';
 import { SwitchTheme } from './components/SwitchTheme';
@@ -11,6 +12,7 @@ const App = () => {
       <IntroCard />
       <TechCard />
       <ProjectCard />
+      <Footer />
 
       <SwitchTheme />
       <Background position='left' image='bg-left.png' />
---
+++ src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+  return <div className='flex justify-center text-tiny text-default-500 mt-5'>Copyright 2024 hellolin. All rights reserved.</div>;
+};
