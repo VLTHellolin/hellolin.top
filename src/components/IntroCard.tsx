4653da495817ace8572c697bae5b85487c7b3b04
VLTHellolin <i@hellolin.top>
2024-12-29 00:42:26 +0800

    feat: add animations for the card

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -4,6 +4,7 @@ import { Button } from '@nextui-org/button';
 import { Card, CardBody, CardFooter, CardHeader } from '@nextui-org/card';
 import { Chip } from '@nextui-org/chip';
 import { Link } from '@nextui-org/link';
+import { motion } from 'motion/react';
 
 const HobbyChip = ({ text }: { text: string }) => {
   return (
@@ -40,39 +41,41 @@ const SocialButton = ({ href, icon }: { href: string; icon: string }) => {
 
 export const IntroCard = () => {
   return (
-    <Card className='p-4'>
-      <CardHeader className='justify-between'>
-        <div className='flex gap-3'>
-          <Avatar isBordered size='md' src='avatar.png' />
-          <div className='flex flex-col justify-center items-start'>
-            <h4 className='text-medium font-semibold text-default-600'>hellolin</h4>
-            <h5 className='text-small text-default-500'>@VLTHellolin</h5>
+    <motion.div initial={{ y: 200, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.2, duration: 0.8, ease: 'easeOut' }}>
+      <Card className='p-4'>
+        <CardHeader className='justify-between'>
+          <div className='flex gap-3'>
+            <Avatar isBordered size='md' src='avatar.png' />
+            <div className='flex flex-col justify-center items-start'>
+              <h4 className='text-medium font-semibold text-default-600'>hellolin</h4>
+              <h5 className='text-small text-default-500'>@VLTHellolin</h5>
+            </div>
           </div>
-        </div>
 
-        <LongSocialButton href='https://github.com/VLTHellolin' icon='simple-icons:github' text='GitHub' />
-      </CardHeader>
+          <LongSocialButton href='https://github.com/VLTHellolin' icon='simple-icons:github' text='GitHub' />
+        </CardHeader>
 
-      <CardBody className='text-small text-default-500'>
-        A Frontend Developer, Competitive Programmer and UI/UX enthusiast from China.
-        <div className='flex mt-2 gap-1'>
-          <HobbyChip text='14 y.o.' />
-          <HobbyChip text='He/Him' />
-          <HobbyChip text='ENFP-T' />
-          <HobbyChip text='ACG Lover' />
-        </div>
-      </CardBody>
+        <CardBody className='text-small text-default-500'>
+          A Frontend Developer, Competitive Programmer and UI/UX enthusiast from China.
+          <div className='flex mt-2 gap-1'>
+            <HobbyChip text='14 y.o.' />
+            <HobbyChip text='He/Him' />
+            <HobbyChip text='ENFP-T' />
+            <HobbyChip text='ACG Lover' />
+          </div>
+        </CardBody>
 
-      <CardFooter>
-        <div className='flex gap-1 ml-auto'>
-          <SocialButton href='https://x.com/VLTHellolin' icon='simple-icons:x' />
-          <SocialButton href='mailto:i@hellolin.top' icon='material-symbols:stacked-email-outline-rounded' />
-          <SocialButton href='https://t.me/VLTHellolin' icon='simple-icons:telegram' />
-          <SocialButton href='https://www.zhihu.com/people/hellolin.cf' icon='simple-icons:zhihu' />
-          <SocialButton href='https://steamcommunity.com/id/hellolin/' icon='simple-icons:steam' />
-          <SocialButton href='https://discord.com/users/1060072430355370094' icon='simple-icons:discord' />
-        </div>
-      </CardFooter>
-    </Card>
+        <CardFooter>
+          <div className='flex gap-1 ml-auto'>
+            <SocialButton href='https://x.com/VLTHellolin' icon='simple-icons:x' />
+            <SocialButton href='mailto:i@hellolin.top' icon='material-symbols:stacked-email-outline-rounded' />
+            <SocialButton href='https://t.me/VLTHellolin' icon='simple-icons:telegram' />
+            <SocialButton href='https://www.zhihu.com/people/hellolin.cf' icon='simple-icons:zhihu' />
+            <SocialButton href='https://steamcommunity.com/id/hellolin/' icon='simple-icons:steam' />
+            <SocialButton href='https://discord.com/users/1060072430355370094' icon='simple-icons:discord' />
+          </div>
+        </CardFooter>
+      </Card>
+    </motion.div>
   );
 };
